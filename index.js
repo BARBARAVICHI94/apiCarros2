cd6c8c706bc9c0aa104d650e2f7216a3f8db8985
BARBARAVICHI94 <b.vichi@hotmail.com>
2022-05-13 20:33:20 -0300

    alteração pelo terminal

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-qual quer coisa
+//git add . (ponto = todos os arquivos) git add nome do arquivo (para apenas um arquivo)
+//ele é para add ele as alterações preparadas 
+//commit é para subir alterações localmente
+//push subir alterações que eu tenho localnete para o gi (colocar na web)
+//commit = git commit -m "mensagem"
+//git push (sobe)
